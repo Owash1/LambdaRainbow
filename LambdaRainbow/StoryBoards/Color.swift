f78f61d588158e0367396efefc70a2b90dbcccd8
Osha Washington <washingtonosha209@yahoo.com>
2019-11-13 00:19:05 -0800

    Set initial views

---
+++ LambdaRainbow/StoryBoards/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  LambdaRainbow
+//
+//  Created by Osha Washington on 11/12/19.
+//  Copyright © 2019 Osha Washington. All rights reserved.
+//
+
+import UIKit
+
+
+struct MyColor {
+    let name: String
+    let color: UIColor
+}
